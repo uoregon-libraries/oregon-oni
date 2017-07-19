94f45b797d4c82a75bb558d0b8ae63b53c2c2c0b
Jeremy Echols <jechols@uoregon.edu>
2017-07-19 08:29:04 -0700

    Change the scope of the "cities list"
    Titles by location gives us a little more flexibility with how we handle
    situations like titles that have no city

--- urls.py
+++ urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url, include
 import views
 
 urlpatterns = [
-  url(r'^cities$', views.cities, name="oregon_cities"),
+    url(r'^titles_by_location$', views.titles_by_location, name="oregon_titles_by_location"),
 ]
--- views.py
+++ views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django import template
 from django.http import Http404, HttpResponse
 
-def cities(request):
-    page_title = "All Cities"
-    return render(request, "or-cities.html", locals())
+def titles_by_location(request):
+    page_title = "Titles By Location"
+    return render(request, "or-locations.html", locals())
