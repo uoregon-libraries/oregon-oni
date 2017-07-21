26fabfe55b5be6507ce65864fbdfd47dfd1dfd1d
Jeremy Echols <jechols@uoregon.edu>
2017-07-21 14:17:08 -0700

    Add generated cities_list.js for map

---
+++ static/js/cities_list.js
@@ -0,0 +1,824 @@
+var cities = [
+  {
+    "latlong": [
+      44.63651, 
+      -123.10593
+    ], 
+    "name": "Albany", 
+    "papers": {
+      "The State rights democrat.": "sn84022644", 
+      "The Albany register.": "sn84022643"
+    }
+  }, 
+  {
+    "latlong": [
+      44.77487, 
+      -117.83438
+    ], 
+    "name": "Baker City", 
+    "papers": {
+      "Bedrock democrat.": "sn84022647", 
+      "The Baker County press. volume": "2015260133"
+    }
+  }, 
+  {
+    "latlong": [
+      44.02655, 
+      -116.96294
+    ], 
+    "name": "Ontario", 
+    "papers": {
+      "The Ontario Argus.": "sn00063520"
+    }
+  }, 
+  {
+    "latlong": [
+      45.30012, 
+      -122.97316
+    ], 
+    "name": "Newberg", 
+    "papers": {
+      "Newberg graphic.": "sn96088233"
+    }
+  }, 
+  {
+    "latlong": [
+      44.76905, 
+      -117.16851
+    ], 
+    "name": "Richland", 
+    "papers": {
+      "Eagle Valley news.": "sn96088408"
+    }
+  }, 
+  {
+    "latlong": [
+      44.63345, 
+      -121.12949
+    ], 
+    "name": "Madras", 
+    "papers": {
+      "The Madras pioneer.": "sn84024878"
+    }
+  }, 
+  {
+    "latlong": [
+      45.00512, 
+      -122.78315
+    ], 
+    "name": "Silverton", 
+    "papers": {
+      "Appeal tribune.": "2004260523"
+    }
+  }, 
+  {
+    "latlong": [
+      43.3665, 
+      -124.21789
+    ], 
+    "name": "Coos Bay", 
+    "papers": {
+      "Weekly coast mail.": "sn96088442", 
+      "The Coast mail.": "sn96088440", 
+      "The Coos Bay times.": "sn85033159", 
+      "Daily coast mail.": "sn96088441"
+    }
+  }, 
+  {
+    "latlong": [
+      44.3896, 
+      -118.95023
+    ], 
+    "name": "Canyon City", 
+    "papers": {
+      "Grant County news.": "sn85042403"
+    }
+  }, 
+  {
+    "latlong": [
+      45.18234, 
+      -122.80787
+    ], 
+    "name": "Hubbard", 
+    "papers": {
+      "Hubbard enterprise.": "sn97071102"
+    }
+  }, 
+  {
+    "latlong": [
+      45.864, 
+      -122.80649
+    ], 
+    "name": "Saint Helens", 
+    "papers": {
+      "The Columbian.": "sn93051654", 
+      "The Columbia register.": "sn97071109", 
+      "St. Helens mist.": "2004260419"
+    }
+  }, 
+  {
+    "latlong": [
+      43.98211, 
+      -117.23823
+    ], 
+    "name": "Vale", 
+    "papers": {
+      "Malheur enterprise.": "sn00063530"
+    }
+  }, 
+  {
+    "latlong": [
+      45.7054, 
+      -121.52146
+    ], 
+    "name": "Hood River", 
+    "papers": {
+      "The Hood River glacier.": "sn97071110", 
+      "The Hood River news.": "sn83009939", 
+      "The Hood River sun. volume": "2015260100"
+    }
+  }, 
+  {
+    "latlong": [
+      44.62151, 
+      -123.93845
+    ], 
+    "name": "Toledo", 
+    "papers": {
+      "Lincoln County leader.": "sn85033162"
+    }
+  }, 
+  {
+    "latlong": [
+      45.06011, 
+      -123.60928
+    ], 
+    "name": "Grand Ronde", 
+    "papers": {
+      "Smoke signals.": "sn93050714"
+    }
+  }, 
+  {
+    "latlong": [
+      44.56457, 
+      -123.26204
+    ], 
+    "name": "Corvallis", 
+    "papers": {
+      "The Benton democrat. volume": "sn84022649", 
+      "The Corvallis times.": "sn2002060538", 
+      "The Gazette-times.": "sn96088340", 
+      "The Union gazette.": "sn93051659", 
+      "The Oregon union.": "sn96088334", 
+      "The Corvallis gazette.": "sn84022650", 
+      "Corvallis daily gazette. volume": "2014260100", 
+      "The daily gazette-times.": "2004260241", 
+      "Oregon union.": "sn85042402", 
+      "Corvallis gazette.": "sn93051660"
+    }
+  }, 
+  {
+    "latlong": [
+      45.52289, 
+      -122.98983
+    ], 
+    "name": "Hillsboro", 
+    "papers": {
+      "The Hillsboro argus.": "sn84006724", 
+      "The Argus.": "sn96088160", 
+      "Washington County independent.": "sn93051620", 
+      "Washington independent.": "sn84022654", 
+      "Hillsboro independent.": "sn96088159", 
+      "The independent.": "sn93051621"
+    }
+  }, 
+  {
+    "latlong": [
+      44.04624, 
+      -123.02203
+    ], 
+    "name": "Springfield", 
+    "papers": {
+      "The Springfield news.": "sn97071001", 
+      "The Lane County news.": "sn97071002"
+    }
+  }, 
+  {
+    "latlong": [
+      44.05207, 
+      -123.08675
+    ], 
+    "name": "Eugene", 
+    "papers": {
+      "Broad-axe.": "sn96088146", 
+      "The state Republican.": "sn83025130", 
+      "Broad-axe tribune.": "sn96088145", 
+      "The Eugene City guard.": "sn84022653"
+    }
+  }, 
+  {
+    "latlong": [
+      45.35432, 
+      -117.22961
+    ], 
+    "name": "Joseph", 
+    "papers": {
+      "Wallowa chieftain.": "sn94052752"
+    }
+  }, 
+  {
+    "latlong": [
+      45.48706, 
+      -122.80371
+    ], 
+    "name": "Beaverton", 
+    "papers": {
+      "Beaverton times.": "sn96088374", 
+      "The owl.": "sn96088375"
+    }
+  }, 
+  {
+    "latlong": [
+      44.84845, 
+      -123.23399
+    ], 
+    "name": "Monmouth", 
+    "papers": {
+      "Polk County observer.": "sn96088088", 
+      "The Monmouth herald.": "sn96088093"
+    }
+  }, 
+  {
+    "latlong": [
+      45.23095, 
+      -122.75593
+    ], 
+    "name": "Aurora", 
+    "papers": {
+      "Aurora observer.": "sn96088478", 
+      "The Aurora borealis.": "sn96088477"
+    }
+  }, 
+  {
+    "latlong": [
+      44.91928, 
+      -123.31705
+    ], 
+    "name": "Dallas", 
+    "papers": {
+      "Polk County times.": "sn93051615", 
+      "Polk County itemizer observer.": "sn96088087", 
+      "The Polk County signal.": "sn93051616", 
+      "Polk County observer.": "sn96088088", 
+      "Polk County itemizer.": "sn94049693"
+    }
+  }, 
+  {
+    "latlong": [
+      43.65401, 
+      -123.81677
+    ], 
+    "name": "Scottsburg", 
+    "papers": {
+      "The Umpqua weekly gazette.": "sn84022684", 
+      "Umpqua gazette.": "sn98068707"
+    }
+  }, 
+  {
+    "latlong": [
+      46.18788, 
+      -123.83125
+    ], 
+    "name": "Astoria", 
+    "papers": {
+      "Toveritar.": "2011260133", 
+      "Tri-weekly Astorian.": "sn96061148", 
+      "The Daily Astorian.": "sn96061149", 
+      "The daily morning Astorian.": "sn96061150", 
+      "The daily Astorian.": "sn83008376", 
+      "The morning Astorian.": "sn85042400"
+    }
+  }, 
+  {
+    "latlong": [
+      42.57764, 
+      -121.86613
+    ], 
+    "name": "Chiloquin", 
+    "papers": {
+      "Klamath tribune.": "2004260350"
+    }
+  }, 
+  {
+    "latlong": [
+      45.59456, 
+      -121.17868
+    ], 
+    "name": "Dalles", 
+    "papers": {
+      "The Dalles times-mountaineer.": "sn93051669", 
+      "The Dalles weekly chronicle.": "2003260222", 
+      "The Dalles daily chronicle.": "sn85042448", 
+      "The daily mountaineer.": "sn89055266"
+    }
+  }, 
+  {
+    "latlong": [
+      45.84041, 
+      -119.28946
+    ], 
+    "name": "Hermiston", 
+    "papers": {
+      "Hermiston herald.": "sn97071047"
+    }
+  }, 
+  {
+    "latlong": [
+      44.83431, 
+      -119.79445
+    ], 
+    "name": "Spray", 
+    "papers": {
+      "Spray courier.": "sn97071004"
+    }
+  }, 
+  {
+    "latlong": [
+      42.32652, 
+      -122.87559
+    ], 
+    "name": "Medford", 
+    "papers": {
+      "Medford mail tribune.": "97071090"
+    }
+  }, 
+  {
+    "latlong": [
+      44.29095, 
+      -121.54921
+    ], 
+    "name": "Sisters", 
+    "papers": {
+      "The nugget.": "2001260330"
+    }
+  }, 
+  {
+    "latlong": [
+      44.76345, 
+      -121.26616
+    ], 
+    "name": "Warm Springs", 
+    "papers": {
+      "Spilyay tymoo.": "sn93050507"
+    }
+  }, 
+  {
+    "latlong": [
+      43.17705, 
+      -124.18761
+    ], 
+    "name": "Coquille", 
+    "papers": {
+      "Coquille City herald.": "sn93051617"
+    }
+  }, 
+  {
+    "latlong": [
+      43.58626, 
+      -119.0541
+    ], 
+    "name": "Burns", 
+    "papers": {
+      "The Times-herald.": "sn96088246"
+    }
+  }, 
+  {
+    "latlong": [
+      42.31346, 
+      -122.96699
+    ], 
+    "name": "Jacksonville", 
+    "papers": {
+      "The Table Rock sentinel.": "sn84022658", 
+      "Oregon sentinel.": "sn84022657"
+    }
+  }, 
+  {
+    "latlong": [
+      42.22487, 
+      -121.78167
+    ], 
+    "name": "Klamath Falls", 
+    "papers": {
+      "The Evening herald.": "sn99063812"
+    }
+  }, 
+  {
+    "latlong": [
+      45.83986, 
+      -119.70058
+    ], 
+    "name": "Boardman", 
+    "papers": {
+      "The Boardman mirror.": "sn96088002"
+    }
+  }, 
+  {
+    "latlong": [
+      45.35318, 
+      -119.5578
+    ], 
+    "name": "Heppner", 
+    "papers": {
+      "Heppner weekly gazette.": "sn97071041", 
+      "Heppner herald.": "sn97071037", 
+      "Heppner gazette-times.": "sn97071042", 
+      "The Gazette-times.": "sn97071038", 
+      "Heppner gazette.": "sn94049698", 
+      "The Weekly Heppner gazette.": "sn94049697", 
+      "Heppner times.": "sn97071040"
+    }
+  }, 
+  {
+    "latlong": [
+      43.4065, 
+      -124.22428
+    ], 
+    "name": "North Bend", 
+    "papers": {
+      "The Coos Bay times.": "sn85033159"
+    }
+  }, 
+  {
+    "latlong": [
+      42.89066, 
+      -124.46261
+    ], 
+    "name": "Denmark", 
+    "papers": {
+      "Southwest Oregon recorder.": "sn96088349"
+    }
+  }, 
+  {
+    "latlong": [
+      45.99316, 
+      -123.92264
+    ], 
+    "name": "Seaside", 
+    "papers": {
+      "Seaside signal.": "sn99063826"
+    }
+  }, 
+  {
+    "latlong": [
+      45.44513, 
+      -119.68447
+    ], 
+    "name": "Lexington", 
+    "papers": {
+      "Lexington wheatfield.": "sn97071052", 
+      "Lexington weekly budget.": "sn97071051"
+    }
+  }, 
+  {
+    "latlong": [
+      43.79762, 
+      -123.05952
+    ], 
+    "name": "Cottage Grove", 
+    "papers": {
+      "Bohemia nugget.": "sn96088074", 
+      "Cottage Grove sentinel.": "sn96088073"
+    }
+  }, 
+  {
+    "latlong": [
+      45.20847, 
+      -117.86521
+    ], 
+    "name": "Union", 
+    "papers": {
+      "The Oregon scout.": "sn93051670"
+    }
+  }, 
+  {
+    "latlong": [
+      44.29985, 
+      -120.83447
+    ], 
+    "name": "Prineville", 
+    "papers": {
+      "Prineville review.": "sn00063658", 
+      "Crook County journal.": "sn00063661", 
+      "Ochoco review.": "sn00063648"
+    }
+  }, 
+  {
+    "latlong": [
+      43.87683, 
+      -116.99488
+    ], 
+    "name": "Nyssa", 
+    "papers": {
+      "Nyssa gate city journal.": "sn99063863", 
+      "The Gate city journal.": "sn99063862"
+    }
+  }, 
+  {
+    "latlong": [
+      45.8118, 
+      -118.49053
+    ], 
+    "name": "Athena", 
+    "papers": {
+      "The Athena press.": "sn96088356"
+    }
+  }, 
+  {
+    "latlong": [
+      45.32458, 
+      -118.08772
+    ], 
+    "name": "La Grande", 
+    "papers": {
+      "La Grande evening observer.": "2006260039"
+    }
+  }, 
+  {
+    "latlong": [
+      45.42626, 
+      -117.27878
+    ], 
+    "name": "Enterprise", 
+    "papers": {
+      "Enterprise news-record.": "sn96088044", 
+      "The News=record.": "sn96088043", 
+      "Wallowa chieftain.": "sn94052752", 
+      "Wallowa County chieftain.": "sn90057139"
+    }
+  }, 
+  {
+    "latlong": [
+      42.19458, 
+      -122.70948
+    ], 
+    "name": "Ashland", 
+    "papers": {
+      "Ashland daily tidings.": "sn96088003", 
+      "Ashland tidings.": "sn85042399", 
+      "Rogue news.": "2013260024", 
+      "Ashland weekly tidings.": "sn96088004"
+    }
+  }, 
+  {
+    "latlong": [
+      44.41599, 
+      -118.95301
+    ], 
+    "name": "John Day", 
+    "papers": {
+      "Grant County news.": "sn85042403", 
+      "The Blue Mountain eagle.": "sn96088264"
+    }
+  }, 
+  {
+    "latlong": [
+      45.17512, 
+      -121.08145
+    ], 
+    "name": "Maupin", 
+    "papers": {
+      "The Maupin times.": "sn96088123"
+    }
+  }, 
+  {
+    "latlong": [
+      44.74571, 
+      -118.20216
+    ], 
+    "name": "Sumpter", 
+    "papers": {
+      "The Sumpter miner.": "sn96088466"
+    }
+  }, 
+  {
+    "latlong": [
+      45.52345, 
+      -122.67621
+    ], 
+    "name": "Portland", 
+    "papers": {
+      "Willamette farmer.": "sn85042522", 
+      "St. Johns review.": "sn00063676", 
+      "Portland observer.": "sn83025151", 
+      "The Oregonian.": "sn83045780", 
+      "The Oregon daily journal.": "sn85042444", 
+      "The new age.": "sn83025107", 
+      "The skanner.": "sn85042482", 
+      "Morning Oregonian.": "sn83025138", 
+      "The illustrated west shore.": "2012260365", 
+      "The west shore.": "2012260361", 
+      "The Asian reporter.": "sn97001795", 
+      "Willamette week.": "sn88087070", 
+      "Portland new age.": "sn83025137", 
+      "The new Northwest.": "sn84022673", 
+      "The Sunday Oregonian.": "sn83045782"
+    }
+  }, 
+  {
+    "latlong": [
+      45.89177, 
+      -123.96153
+    ], 
+    "name": "Cannon Beach", 
+    "papers": {
+      "Cannon Beach gazette.": "sn96088385"
+    }
+  }, 
+  {
+    "latlong": [
+      42.43933, 
+      -123.33067
+    ], 
+    "name": "Grants Pass", 
+    "papers": {
+      "Rogue River courier.": "sn96088180", 
+      "Grants Pass daily courier.": "sn96088181"
+    }
+  }, 
+  {
+    "latlong": [
+      44.85123, 
+      -123.18677
+    ], 
+    "name": "Independence", 
+    "papers": {
+      "The Independence west side.": "2011260136", 
+      "West side enterprise.": "sn96088099", 
+      "Independence monitor.": "2012260081", 
+      "The Polk County post.": "2012260080", 
+      "Independence enterprise.": "sn96088097"
+    }
+  }, 
+  {
+    "latlong": [
+      44.53651, 
+      -122.90703
+    ], 
+    "name": "Lebanon", 
+    "papers": {
+      "The Lebanon express.": "sn97071028"
+    }
+  }, 
+  {
+    "latlong": [
+      45.81374, 
+      -118.42469
+    ], 
+    "name": "Weston", 
+    "papers": {
+      "The Weston leader. volume": "sn94052320", 
+      "Weston weekly leader.": "sn94052319"
+    }
+  }, 
+  {
+    "latlong": [
+      45.4564, 
+      -123.84553
+    ], 
+    "name": "Tillamook", 
+    "papers": {
+      "Tillamook herald.": "sn99063850"
+    }
+  }, 
+  {
+    "latlong": [
+      42.18877, 
+      -120.34579
+    ], 
+    "name": "Lakeview", 
+    "papers": {
+      "Lake County examiner.": "sn97071141"
+    }
+  }, 
+  {
+    "latlong": [
+      44.9429, 
+      -123.0351
+    ], 
+    "name": "Salem", 
+    "papers": {
+      "Willamette farmer.": "sn85042522", 
+      "Capital press. [West]": "2007260205", 
+      "The Chemawa American [microform].": "2003238611", 
+      "Capital journal.": "sn90066132", 
+      "Keizertimes.": "sn00063621", 
+      "The Oregon Argus.": "sn83025129", 
+      "Evening capital journal.": "sn99063952", 
+      "Weekly Chemawa American.": "ca15001324", 
+      "Daily capital journal.": "sn99063955", 
+      "The Daily journal.": "sn99063956", 
+      "Capital press. [East]": "2007260204"
+    }
+  }, 
+  {
+    "latlong": [
+      44.05817, 
+      -121.31531
+    ], 
+    "name": "Bend", 
+    "papers": {
+      "The Bend bulletin.": "sn96088235"
+    }
+  }, 
+  {
+    "latlong": [
+      45.35734, 
+      -122.60676
+    ], 
+    "name": "Oregon City", 
+    "papers": {
+      "Morning enterprise.": "sn00063701", 
+      "Oregon City courier=herald.": "sn00063697", 
+      "The enterprise.": "sn94052322", 
+      "Oregon City enterprise.": "sn94052321", 
+      "The banner-courier.": "sn00063699", 
+      "Oregon free press.": "sn84022661", 
+      "Oregon City courier.": "sn00063694", 
+      "The Weekly enterprise.": "sn85042452", 
+      "Clackamas County record.": "sn00063710", 
+      "Oregon spectator.": "sn84022662", 
+      "Oregon City press.": "2012260084", 
+      "The Oregon Argus.": "sn83025129", 
+      "Oregon courier.": "sn00063695"
+    }
+  }, 
+  {
+    "latlong": [
+      45.50124, 
+      -119.82475
+    ], 
+    "name": "Ione", 
+    "papers": {
+      "The Ione proclaimer.": "sn97071034", 
+      "The Ione independent.": "sn97071039", 
+      "The Ione journal.": "sn97071035", 
+      "The Ione bulletin.": "sn97071036"
+    }
+  }, 
+  {
+    "latlong": [
+      43.119, 
+      -124.40845
+    ], 
+    "name": "Bandon", 
+    "papers": {
+      "Semi-weekly Bandon recorder.": "sn96088347", 
+      "The Bandon recorder.": "sn96088348", 
+      "Bandon recorder.": "sn93051610"
+    }
+  }, 
+  {
+    "latlong": [
+      44.99012, 
+      -123.02621
+    ], 
+    "name": "Keizer", 
+    "papers": {
+      "Keizertimes.": "sn00063621"
+    }
+  }, 
+  {
+    "latlong": [
+      43.2165, 
+      -123.34174
+    ], 
+    "name": "Roseburg", 
+    "papers": {
+      "The Plaindealer.": "sn84022679", 
+      "The Douglas independent.": "sn93051662", 
+      "Roseburg review.": "sn93051663"
+    }
+  }, 
+  {
+    "latlong": [
+      43.98262, 
+      -124.09984
+    ], 
+    "name": "Florence", 
+    "papers": {
+      "The Siuslaw news.": "sn96004772"
+    }
+  }, 
+  {
+    "latlong": [
+      45.67207, 
+      -118.7886
+    ], 
+    "name": "Pendleton", 
+    "papers": {
+      "East Oregonian : E.O.": "sn88086023", 
+      "The east Oregonian.": "sn84022663"
+    }
+  }
+];
