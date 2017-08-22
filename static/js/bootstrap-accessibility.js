4db55039828b190d63fb576109e2f8fecc94cfa2
Jeremy Echols <jechols@uoregon.edu>
2017-08-22 15:03:08 -0700

    Remove modal extension from bootstrap a11y script
    Causes a JS error

--- static/js/bootstrap-accessibility.js
+++ static/js/bootstrap-accessibility.js
@@ -92,30 +92,6 @@ jQuery(document).ready(function() {
     }
   });
 
-  // Modal Extension
-  // ===============================
-
-	$('.modal-dialog').attr( {'role' : 'document'})
-    var modalhide =   $.fn.modal.Constructor.prototype.hide
-    $.fn.modal.Constructor.prototype.hide = function(){
-       modalhide.apply(this, arguments)
-       $(document).off('keydown.bs.modal')
-    }
-
-    var modalfocus =   $.fn.modal.Constructor.prototype.enforceFocus
-    $.fn.modal.Constructor.prototype.enforceFocus = function(){
-      var focEls = this.$element.find(":tabbable")
-        , lastEl = focEls[focEls.length-1]
-      $(document).on('keydown.bs.modal', $.proxy(function (ev) {
-        if(!this.$element.has(ev.target).length && ev.shiftKey && ev.keyCode === 9) {  
-          lastEl.focus()
-          ev.preventDefault();
-        }
-      }, this))
-
-      modalfocus.apply(this, arguments)
-    }    
-
   // DROPDOWN Extension
   // ===============================
   
