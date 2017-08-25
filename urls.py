59b9640ccad8f6c8f2597f6c8eb1474d2faf479d
Jeremy Echols <jechols@uoregon.edu>
2017-08-25 09:43:07 -0700

    Remove titles by location from theme

--- urls.py
+++ urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url, include
+from onisite.plugins.title_locations import views as tl_views
 import views
 
 urlpatterns = [
-    url(r'^titles_by_location$', views.titles_by_location, name="oregon_titles_by_location"),
+    url(r'^titles_by_location$', tl_views.titles_by_location, name="oregon_titles_by_location"),
     url(r'^$', views.home),
 ]
